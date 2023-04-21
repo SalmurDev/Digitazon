5612a8e4e03d8e59452a9d817a278ab44f2f75b3
SalmurDev <filippo.bortolo@outlook.com>
2023-04-21 17:26:53 +0200

    test

--- esercitazioni/modulo2/04_20/esercizio4.js
+++ esercitazioni/modulo2/04_20/esercizio4.js
@@ -1,10 +1,54 @@
-function tri(n) {
-    for (let i = 1; i < 2*n; i++) {
-        let a = ''
-        for (let j = 0; j < n-Math.abs(n - i) ; j++) {
-            a += 'x'
-        }
-        console.log(a)
-    }
+// primo tentativo
+
+// function tri(n) {
+//     for (let i = 1; i < 2*n; i++) {
+//         let a = ''
+//         for (let j = 0; j < n-Math.abs(n - i) ; j++) {
+//             a += 'x'
+//         }
+//         console.log(a)
+//     }
+// }
+// tri(4)
+
+// secondo tentativo
+
+// function triR(n) {
+//     if (n <= 0) return ;
+// else {
+//         let a = '';
+//         for (let i = 0; i < n; i++) {
+//             a += 'x';
+//         }
+//         return console.log(a)+ triR(n-1);
+//     }
+// }
+// function tri(n) {
+//     if (n <= 0) return;
+//     else {
+//         let a = '';
+//         for (let i = 0; i < n; i++) {
+//             a += 'x';
+//         }
+//         return tri(n-1)+ console.log(a);
+//     }
+// }
+// function pyramid(n){
+//   return tri (n) + triR(n-1);
+//   }
+// pyramid(5);
+
+// terzo tentativo
+
+function triR(n) {
+    if (n <= 0) return
+    else return console.log('x'.repeat(n)) + triR(n - 1)
 }
-tri(4)
+function tri(n) {
+    if (n <= 0) return;
+    else return tri(n - 1) + console.log('x'.repeat(n))
+}
+function pyramid(n) {
+    return tri(n) + triR(n - 1)
+}
+pyramid(7);
---
+++ esercitazioni/modulo2/Test/test2.js
@@ -0,0 +1,51 @@
+// 2 - Find and update
+// Scrivere una funzione che, dato in ingresso un array di oggetti così strutturato:
+// sia in grado di attribuire un valore di default dove sia presente un null, seguendo queste
+// regole:
+// ● se il type e’ “boolean” deve aggiornare usando false
+// ● se il type e’ “string” deve aggiornare usando stringa vuota
+// ● se il type e’ “number” deve aggiornare usando 0
+// ● se il type e’ “array” deve aggiornare usando array vuoto
+// ● se il type e’ “object” deve aggiornare usando oggetto vuoto
+// Come vedete ogni singolo oggetto ha sempre due attributi, uno la cui chiave non e’ dato a
+// sapere a priori, un altro la cui chiave e’ sempre “type” e il valore e’ nella lista qui sopra.
+// La funzione deve ritornare lo stesso oggetto ricevuto in input, con i valori aggiornati.
+
+let array = [
+    {
+        'maggiorenne': null,
+        'type': 'boolean'
+    }, {
+        'nome': null,
+        'type': 'string'
+    }, {
+        'cognome': 'Rossi',
+        'type': 'string'
+    }, {
+        'voti': null,
+        'type': 'array'
+    }, {
+        'media': null,
+        'type': 'number'
+    }, {
+        'presenze': null,
+        'type': 'object'
+    }
+
+]
+
+function findUpdate(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (const key in arr[i]) {
+            if (arr[i][key] == null) {
+                if (arr[i].type == 'boolean') arr[i][key]  = false
+                if (arr[i].type == 'string') arr[i][key] = ''
+                if (arr[i].type == 'number') arr[i][key]  = 0
+                if (arr[i].type == 'array') arr[i][key]  = []
+                if (arr[i].type == 'object') arr[i][key]  = {}
+            }
+        }
+    }
+    return arr
+}
+console.log(findUpdate(array));
