8683725567f791b26565c36452b01cce62c8dc80
SalmurDev <filippo.bortolo@outlook.com>
2023-04-27 17:10:45 +0200

    k

--- esercitazioni/modulo2/04_27/pascal.js
+++ esercitazioni/modulo2/04_27/pascal.js
@@ -1,17 +1,12 @@
 // creare  il triangolo di pascal senza spazi
 
-// funzione che fa una singola somma
-function sum(a, b) {
-    return a + b
-}
-
 // funzione che prende un array di numeri, e ritorna una stringa
 // con un numero in più e tutti i numeri sommati
 function line(arr) {
     const array2 = []
     array2.push(1)
     for (let i = 1; i < arr.length; i++) {
-        array2.push(sum(arr[i], arr[i - 1]))
+        array2.push(arr[i] + arr[i - 1])
     }
     array2.push(1)
     return array2
@@ -22,9 +17,8 @@ function pascal(n) {
     let arr = [1]
     console.log(arr)
     for (let i = 0; i < n; i++) {
-
         arr = line(arr)
         console.log(arr)
     }
 }
-pascal(5)
+pascal(7)
