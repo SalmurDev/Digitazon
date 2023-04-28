f2c8ce4f20fe6d9b6d8d61db4260b05f8b5dfb0d
SalmurDev <filippo.bortolo@outlook.com>
2023-04-28 14:28:51 +0200

    k

--- esercitazioni/modulo2/Test/test3.js
+++ esercitazioni/modulo2/Test/test3.js
@@ -32,14 +32,32 @@ let tree = {
         'right': 'null'
     }
 }
-//console.log(tree['left']);
-//se è uscito dal ciclo for, 
-//
 
-function findPerson(object,Person){
-    for (const key in object) {
-        if(object.key == Person) return 1
-        if(typeof object.key == 'object') return 1 + findPerson(object[key],Person)
-    } 
-}
-console.log(findPerson(tree,'Earendil'))
+console.log(JSON.stringify(tree).match('{'))
+
+//crea un loop che cicla all'interno dell'oggetto
+//se trova la parola, ritorna 1 + un contatore che parte da 0
+//se trova un oggetto, cicla nuovamente al suo interno e aggiunge 1 al contatore
+//continua in questo modo finchè non trova la parola
+//ad ogni iterazione del loop, se non ha trovato la parola, il contatore torna a 0
+//se non la trova mai ritorna -1
+
+
+// function findPerson(object, person, count) {    
+//     for (const key in object) {
+//         if (object[key] == person) return count
+//         if (typeof object[key] == 'object') 
+//             findPerson(object[key], person, count+1)
+//     }
+//     return -1
+// }
+// console.log(findPerson(tree, 'Nimloth the Fair',1))
+
+// function findChar(object, person, depth){
+//     if(!object) return
+//     if(object.value == person) return depth
+//     if(object.left)findChar(object.left, person, depth+1)
+//     if(object.right)findChar(object.right, person, depth+1)
+//     return -1
+// }
+// console.log(findChar(tree, 'Eowin', 1))
